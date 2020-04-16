df874a87bf1eea3fcde9c675b9348193ee999821
IsoaSFlus <me@isoasflus.me>
2020-04-16 17:14:07 +0800

    init

---
+++ config.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := vendor/pingfang
+
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts/NotoSansCJK-Regular.ttc:$(TARGET_COPY_OUT_SYSTEM)/fonts/NotoSansCJK-Regular.ttc \
+    $(LOCAL_PATH)/fonts/Roboto-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Italic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-LightItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-LightItalic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Light.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-MediumItalic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Medium.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Regular.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-ThinItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-ThinItalic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Thin.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Thin.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Black.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-BlackItalic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-BoldItalic.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-Bold.ttf \
+    $(LOCAL_PATH)/fonts/Roboto-BlackItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Roboto-BlackItalic.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-BoldItalic.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-Bold.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-Italic.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-LightItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-LightItalic.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-Light.ttf \
+    $(LOCAL_PATH)/fonts/RobotoCondensed-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/RobotoCondensed-Regular.ttf \
